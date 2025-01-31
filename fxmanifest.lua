a3b9f3c28097b4a447c1c0c5dee6ce11d66245ef
crazy-kol <uskolbee@gmail.com>
2025-01-30 18:04:54 -0800

    EasyAdmin Ox_Lib P1 Still Working On it

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,7 +11,10 @@ lua54 'yes'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-shared_script 'shared/util_shared.lua'
+shared_scripts {
+    'shared/util_shared.lua',
+    '@ox_lib/init.lua'
+} 
 
 server_scripts {
     "server/*.lua",
